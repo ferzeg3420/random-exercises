8f7fd4cf7da42343961a441734445046c21d5a24
Fernando Zegada <ferzeg3420@gmail.com>
2021-03-05 00:36:36 -0400

    added a README

---
+++ README.md
@@ -0,0 +1,10 @@
+# EXERCISES
+
+This is a script that generates random exercises with rep numbers. It takes these exercises from a file that can be maintained by the user through some commands that I put in here.
+
+# Installation
+
+Clone this repository:
+```
+git clone 
+```
--- exer_rc.sh
+++ exer_rc.sh
@@ -1,4 +1,10 @@
+# the name where the DB files will be stored.
+# Right now it's using the directory of the main exec script
 EXER_DB="$(dirname "$0")"
+
+# The actual DB as it were
 EXER_LIST="${EXER_DB}/exercises.list"
+
+#Temp files where I'm keeping the randomized lists
 CLEAN_LIST=/tmp/clean.tmp
 RAND_LIST=/tmp/random_exer_list.tmp
