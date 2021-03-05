2b9dea313a60ef1fad7ff50806d82403953ee3f0
Fernando Zegada <ferzeg3420@gmail.com>
2021-03-05 00:29:12 -0400

    Added more info for the help page. Also added a lot of comments to help me understand my own code

--- exer.sh
+++ exer.sh
@@ -2,11 +2,16 @@
 
 #set -x
 
+# including exer_rc.sh and edit exer modules
 . "$(dirname "$0")/exer_rc.sh"
 . "$(dirname "$0")/edit_exer_db_lib.sh"
 
+# get the name of this executable (script)
 EXEC_NAME=${0##*/}
 EXEC_NAME=${EXEC_NAME/\//}
+
+# Constants
+SUCCESS=0
 
 e_flag=''
 a_flag=''
@@ -15,17 +20,29 @@ num_to_display=1
 
 print_usage() 
 {
-   printf "Usage: $EXEC_NAME [-ea] [-d] [-n [0-9]+ ]\n"
+   echo 
+   printf "Usage: $EXEC_NAME [-eah] [-d] [-n number] ]\n"
+   echo
+   echo "Gives you a random number of reps and an exercise to maintain that"
+   echo "glorius boody"
+   echo
+   echo "    -e Edit the exercise file"
+   echo "    -a Add an exercise using a helper script"
+   echo "    -d Delete an exercise"
+   echo "    -n *number* Get multiple exercises"
+   echo "    -h Print this page"
 }
 
 while getopts 'eadn:' flag
 do
   case "${flag}" 
   in
-    e) e_flag='true' ;;
-    a) a_flag='true' ;;
-    d) d_flag='true' ;;
-    n) num_to_display="${OPTARG}" ;;
+    e) e_flag='true' ;; # Edit an exercise
+    a) a_flag='true' ;; # Add an exercise to the list
+    d) d_flag='true' ;; # Get a description along with the exercise
+    n) num_to_display="${OPTARG}" ;; # Get multiple exercises
+    h) print_usage 
+       exit $SUCCESS ;;
     *) print_usage
        exit 1 ;;
   esac
@@ -34,22 +51,28 @@ done
 if [ "$e_flag" = 'true' ]
 then
    vim "$EXER_LIST"
-   exit 0 
+   exit $SUCCESS 
 fi
 
 if [ "$a_flag" = 'true' ]
 then
    add_exer
-   exit 0
+   exit $SUCCESS 
 fi
 
-# exit if num_to_display is not an integer
+# exit if num_to_display (number of exercises) is not an integer
 (( $num_to_display + 0 )) ||  exit 1
 
+# Preprocess the list of exercises
 (sed '/^[ 	]*$/d' $EXER_LIST | sed '/^[ 	]*#/d') >${CLEAN_LIST}
+
+# Get the number of exercises in the list
 NUM_WORK_OUTS=`wc -l <${CLEAN_LIST}`
+
+# Create a list with the exercises in random order
 (cat ${CLEAN_LIST} | sort -R | head -n ${num_to_display}) >${RAND_LIST}
 
+# Iterate through the list of exercises that is in random order
 while read -r line
 do
    EXER_NAME=$(echo "$line" | cut -d , -f 1)
@@ -68,6 +91,7 @@ do
    
    if [ $RANDOM_REPS = '1' ]
    then
+      # Display the name of the exercise, singular time units
       case "$TYPE" in
       second|minute|hour|day|year|decade|light-year)
          echo "Do a ${EXER_NAME} for 1 ${TYPE}." ;;
@@ -75,6 +99,7 @@ do
          echo "Do 1 ${EXER_NAME}." ;;
       esac
    else
+      # Display the name of the exercise, plural time units
       case "$TYPE" in
       second|minute|hour|day|year|decade|light-year)
          echo "Do a ${EXER_NAME} for ${RANDOM_REPS} ${TYPE}s." ;;
@@ -85,6 +110,7 @@ do
 
    if [ "${d_flag}" = 'true' ]
    then
+      # show the description if user used the description flag
       echo --------------- Description:
       cat "${EXER_DB}/${DESCRIPTION_FILE}" 
       echo ---------------------------
